45e47c5855ec8bb47a2d554faeee786de9c02d62
Madhuri140492 <madhurideshmukh1404@gmail.com>
2022-11-21 20:18:57 +0530

    Hello World Program

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+// FileName : "HelloWorld.java".
+// Write a Java program to display any message:
+
+
+package Day1_PracticeProblem;
+
+public class HelloWorld // Class Declaration, Class name First Initial Caps
+{
+    public static void main(String[] args) // Main Method Declaration
+    {
+        System.out.println("Hello world!"); // Printing the Output
+    }
+}
